b0228d81f647ab0d2f5a41ba2cda1e963b57e3fc
Asadbek <asadbek.rahimov1006@gmail.com>
2022-08-25 14:11:07 +0500

    add PurchaseParty model and migration

---
+++ app/Models/PurchaseParty.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PurchaseParty extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'supplier_id',
+        'price',
+        'user_id',
+        'branch_id',
+    ];
+
+    public function supplier()
+    {
+        return $this->belongsTo(Customer::class, 'supplier_id', 'id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(Customer::class, 'user_id', 'id');
+    }
+
+    public function branch()
+    {
+        return $this->belongsTo(Customer::class, 'branch_id', 'id');
+    }
+}
