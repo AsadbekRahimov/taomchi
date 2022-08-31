93bb4ba8f5155160819f1f0aded8b592708eb026
Asadbek Rahimov <asadbek.rahimov1006@gmail.com>
2022-08-31 01:10:27 -0700

    add product parties list and lists of product screen with route

--- routes/platform.php
+++ routes/platform.php
@@ -168,3 +168,39 @@ Route::screen('stock/sell/{customer}', \App\Orchid\Screens\Sell\MainSellScreen::
             ->parent('platform.index')
             ->push('Maxsulot sotib olish');
     });
+
+// Sotib olingan partiyalar
+Route::screen('stock/buy/parties', \App\Orchid\Screens\Buy\PurchasesPartyScreen::class)
+    ->name('platform.buy_parties')
+    ->breadcrumbs(function (Trail $trail) {
+        return $trail
+            ->parent('platform.index')
+            ->push('Sotib olingan partiyalar');
+    });
+
+// Sotilgan  partiyalar
+Route::screen('stock/sell/parties', \App\Orchid\Screens\Sell\SalesPartyScreen::class)
+    ->name('platform.sell_parties')
+    ->breadcrumbs(function (Trail $trail) {
+        return $trail
+            ->parent('platform.index')
+            ->push('Sotilgan partiyalar');
+    });
+
+// Sotib olingan maxsulotlar
+Route::screen('stock/buy/parties/{party}', \App\Orchid\Screens\Buy\PurchasesScreen::class)
+    ->name('platform.buy_party')
+    ->breadcrumbs(function (Trail $trail) {
+        return $trail
+            ->parent('platform.buy_parties')
+            ->push('Sotib olingan maxsulotlar');
+    });
+
+// Sotilgan  maxsulotlar
+Route::screen('stock/sell/parties/{party}', \App\Orchid\Screens\Sell\SalesScreen::class)
+    ->name('platform.sell_party')
+    ->breadcrumbs(function (Trail $trail) {
+        return $trail
+            ->parent('platform.sell_parties')
+            ->push('Sotilgan partiyalar');
+    });
